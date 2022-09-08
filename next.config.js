bfe9c3640a669c3076cc3f01d4839fb9f329b88c
YaroslavKononenko <106470996+YaroslavKononenko@users.noreply.github.com>
2022-09-08 17:27:44 +0300

    sstep

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: ""
+  },
+  basePath: "/",
+  assetPrefix: "/",
 }
 
 module.exports = nextConfig
