05af152ec9dbc7674c517125835bc09ef8ccbf4b
YaroslavKononenko <106470996+YaroslavKononenko@users.noreply.github.com>
2022-09-08 18:20:57 +0300

    4step

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: ""
-  },
-  basePath: "/nextjs_testdeploy",
-  assetPrefix: "/nextjs_testdeploy",
 }
 
 module.exports = nextConfig
